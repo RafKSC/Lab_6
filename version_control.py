c427a29032f34a499d7f2c0f13caa08f730abfb1
RafKSC <hincheyr@ufl.edu>
2024-03-11 19:10:17 -0400

    Adding the encode and main functions to the repo

--- version_control.py
+++ version_control.py
@@ -1,3 +1,40 @@
-# This is my first github addition
+# Rafael Hinchey
 
-print("Hello Lab 6")
+def encode(password):
+    new_password = ""
+    data = password[:]
+    for i in data:
+        j = int(i)
+        k = (j + 3) % 10
+        x = str(k)
+        new_password += x
+    return new_password
+
+
+def main():
+    encrypt = True
+    while encrypt is True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+        option = int(input("Please enter an option: "))
+
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encode(password)
+            print("Your password has been encoded and stored!")
+
+        if option == 2:
+            pass
+            # code below is option 2 but, we're supposed to create a decode function?
+            # print(f"Your encoded password is {encode(password)}, and the original password is {password}.")
+
+        if option == 3:
+            encrypt = False
+
+
+if __name__ == '__main__':
+    main()
